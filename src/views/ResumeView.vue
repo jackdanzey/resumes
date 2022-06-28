5962f873e6aef2f0e02d001f71b4170fabfa3942
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-06-28 20:49:03 +0200

    finished adding Student Resume and installed bootstrap

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,35 +1,59 @@
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
       message: "RESUME! xxxxx",
-      students: {
+      student: {
         first_name: "Jack",
         last_name: "Danzey",
         email: "jack@test.com",
         phone_number: "2225551111",
-        short_bio: "Actualize student",
-        linkedIn_url: "linkedin.com/jack",
+        short_bio: "Actualize student in AlleyOOP cohort, currently on week 10 of 12!",
+        linkedIn_url: "www.linkedin.com/jack",
         twitter: "@jackdanzey",
-        personal_blog_url: "jack.com",
+        personal_blog_url: "www.jack.com",
         resume: "resume.url",
         github_url: "github.com/jackdanzey",
-        photo: "jack.jpg",
+        photo: "https://i.pinimg.com/564x/1b/14/34/1b1434c7d78bca9e24bcb89e5126903c.jpg",
       },
     };
   },
-  created: function () {},
+  created: function () {
+    axios.get("/students/" + this.$route.params.id + ".json").then((response) => {
+      console.log(response.data);
+      this.students = response.data;
+    });
+  },
   methods: {},
 };
 </script>
 
 <template>
   <div class="home">
-    <h1>{{ students.first_name }} {{ students.last_name }}</h1>
-    <h3>{{ students.email }}</h3>
-    <h3>{{ students.phone_number }}</h3>
-    <h3></h3>
+    <div class="container">
+      <img v-bind:src="student.photo" v-bind:alt="student.first_name" class="image-fit" />
+      <h1>{{ student.first_name }} {{ student.last_name }}</h1>
+      <h2>Contact Information: {{ student.email }} | {{ student.phone_number }} |</h2>
+      <h3>
+        <a :href="`${student.linkedIn_url}`">{{ student.linkedIn_url }}</a>
+        | Twitter: {{ student.twitter }} |
+        <a :href="`${student.personal_blog_url}`">{{ student.personal_blog_url }}</a>
+        |
+        <a :href="`${student.resume}`">{{ student.resume }}</a>
+        |
+        <a :href="`${student.github_url}`">{{ student.github_url }}</a>
+        |
+      </h3>
+      <h2>{{ student.short_bio }}</h2>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style>
+.image-fit {
+  height: 25%;
+  width: 25%;
+  object-fit: cover;
+}
+</style>
