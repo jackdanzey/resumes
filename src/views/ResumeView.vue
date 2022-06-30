b92bca4b33b4fb0b86eadfd2756bc9e6d512a15b
Jack Danzey <jackdanzey@gmail.com>
2022-06-30 08:09:01 -0400

    ?

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -4,40 +4,12 @@ export default {
   data: function () {
     return {
       message: "RESUME! xxxxx",
-      student: {
-        first_name: "Jack",
-        last_name: "Danzey",
-        email: "jack@test.com",
-        phone_number: "2225551111",
-        short_bio: "Actualize student in AlleyOOP cohort, currently on week 10 of 12!",
-        linkedIn_url: "www.linkedin.com/jack",
-        twitter: "@jackdanzey",
-        personal_blog_url: "www.jack.com",
-        resume: "resume.url",
-        github_url: "github.com/jackdanzey",
-        photo: "https://i.pinimg.com/564x/1b/14/34/1b1434c7d78bca9e24bcb89e5126903c.jpg",
-      },
-      capstone: {
-        name: "Super cool fun app",
-        description: "Way cool app that is fun and everything",
-        url: "www.coolapp.com",
-        screenshot: "cool.jpg",
-      },
-      nameFilter: {},
-      experience: {
-        start_date: "Today",
-        end_date: "N/A",
-        job_title: "Smart Guy",
-        company_name: "Syntech",
-        details: "",
-      },
-      education: {
-        start_date: "2010",
-        end_date: "2011",
-        degree: "Yes",
-        university: "Steve Harvard",
-        details: "",
-      },
+      student: {},
+      capstones: [],
+      first_nameFilter: "",
+      experience: {},
+      education: {},
+      skills: [],
     };
   },
   created: function () {
@@ -46,25 +18,25 @@ export default {
       this.students = response.data;
     });
   },
-  methods: {
-    // filterName: function () {
-    // return this.students.filter((student) => {
-    // let lowerName = student.first_name.toLowerCase();
-    // let lowerNameFilter = this.nameFilter.toLowerCase();
-    // return lowerName.includes(lowerNameFilter);
-    // });
-    // },
-  },
+  // methods: {
+  //   filterName: function () {
+  //     return this.students.filter((student) => {
+  //       let lowerName = student.first_name.toLowerCase();
+  //       let lowerNameFilter = this.nameFilter.toLowerCase();
+  //       return lowerName.includes(lowerNameFilter);
+  //     });
+  //   },
+  // },
 };
 </script>
 
 <template>
   <div class="home">
-    <!-- <div v-for="student in filterName()" v-bind:key="student.id"> -->
-    <!-- <h2>{{ student.first_name }}</h2> -->
-    <!-- <p>{{ student.last_name }}</p> -->
-    <!-- <router-link v-bind:to="`/students/${student.id}`">More details</router-link> -->
-    <!-- </div> -->
+    <!-- <div v-for="student in filterName()" v-bind:key="student.id">
+      <h2>{{ student.first_name }}</h2>
+      <p>{{ student.last_name }}</p>
+      <router-link v-bind:to="`/students/${student.id}`">More details</router-link>
+    </div> -->
     <div class="container">
       <img v-bind:src="student.photo" v-bind:alt="student.first_name" class="image-fit" />
       <h1>{{ student.first_name }} {{ student.last_name }}</h1>
